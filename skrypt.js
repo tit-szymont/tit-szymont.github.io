18b6689eb39302a2d340165cdf8fed1f7ac7ff81
tit-szymont <64896865+tit-szymont@users.noreply.github.com>
2020-06-02 19:47:23 +0200

    Skrypt typu java

---
+++ skrypt.js
@@ -0,0 +1,72 @@
+function zmien1()
+{
+    document.getElementById('foto').src="zd1.png";
+}
+function zmien2()
+{
+    document.getElementById('foto').src="zd2.png";
+}
+function zmien3 ()
+{
+        document.getElementById('pies').src="zd3.png";
+}
+function zmien4 ()
+{
+        document.getElementById('pies').src="zd4.png";
+}
+function zmien5 ()
+{
+    document.getElementById('kot').src="zd5.png";
+}
+function zmien6 ()
+{
+    document.getElementById('kot').src="zd6.png";
+}
+function zmien7 ()
+{
+    document.getElementById('zubr').src="zd7.png";
+}
+function zmien8 ()
+{
+    document.getElementById('zubr').src="zd8.png";
+}
+function zmien9 ()
+{
+    document.getElementById('zbik').src="zd9.png";
+}
+function zmien10 ()
+{
+    document.getElementById('zbik').src="zd10.png";
+}
+function zmien11 ()
+{
+    document.getElementById('hipopotam').src="zd11.png";
+}
+function zmien12 ()
+{
+    document.getElementById('hipopotam').src="zd12.png";
+}
+function zmien13 ()
+{
+    document.getElementById('lew').src="zd13.png";
+}
+function zmien14 ()
+{
+    document.getElementById('lew').src="zd14.png";
+}
+function zmien15 ()
+{
+    document.getElementById('niedzwiedz').src="zd15.png";
+}
+function zmien16 ()
+{
+    document.getElementById('niedzwiedz').src="zd16.png";
+}
+function zmien17 ()
+{
+    document.getElementById('hiena').src="zd17.png";
+}
+function zmien18 ()
+{
+    document.getElementById('hiena').src="zd18.png";
+}
